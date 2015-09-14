1c0b1820b8ea9d4f2437a5f0cca23710ae70062a
John Ruffin <john@thiskidcancode.com>
2015-09-14 11:13:59 -0500

    made class level members accessible via Prototype - code reuse, extensibility, reduce namespace risks

--- myWebServer.js
+++ myWebServer.js
@@ -1,22 +1,31 @@
-/*
-The const declaration creates a read-only reference to a value. 
-It does not mean the value it holds is immutable, solely that 
-the variable identifier can not be reassigned.
-*/
-const http = require("http");
+function MyWebServer(){
+}
 
-const myServer = http.createServer(execute_request);
-myServer.listen(8080);
-
-function execute_request(request, response) {
+//all instances will share this method
+MyWebServer.prototype.handle_request = function(req, resp) {
     var body = 'Programmers rock\n';
     var content_length = body.length;
 
-    response.writeHead(200, {
+    resp.writeHead(200, {
         'Content-Length' : content_length,
         'Content-type' : 'text/plain'
     });
-    response.end(body);
+    resp.end(body);
 }
+
+//all instances share http from Node.js 
+MyWebServer.prototype.http = require("http");
+
+//WOW! Already re-using re-usable code in this assignment LOL
+//  no need for instances to do this stuff
+//  yes, I laugh at my own humor
+MyWebServer.prototype.server = 
+    MyWebServer.prototype.http.createServer(MyWebServer.prototype.handle_request);
+
+
+//instantiate a new instance of MyWebServer and port to listen
+var mws = new MyWebServer();
+mws.server.listen(8080);
+
 
 
